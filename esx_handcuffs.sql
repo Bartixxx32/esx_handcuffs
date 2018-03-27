c34edadad676279eaba3ff4227539ac9050b63b0
scorpio686 <31187145+scorpio686@users.noreply.github.com>
2018-03-27 14:42:35 +0200

    Update esx_handcuffs.sql

--- esx_handcuffs.sql
+++ esx_handcuffs.sql
@@ -1,2 +1,2 @@
-INSERT INTO `items` (`id`, `name`, `label`, `limit`, `rare`, `can_remove`) VALUES (NULL, 'handcuffs', 'Handcuffs', -1, 0, 1);
-INSERT INTO `items` (`id`, `name`, `label`, `limit`, `rare`, `can_remove`) VALUES (NULL, 'key', 'Key', -1, 0, 1);
+INSERT INTO `items` (`id`, `name`, `label`, `limit`, `rare`, `can_remove`) VALUES (NULL, 'handcuffs', 'Menottes', -1, 0, 1);
+INSERT INTO `items` (`id`, `name`, `label`, `limit`, `rare`, `can_remove`) VALUES (NULL, 'key', 'Clé des menottes', -1, 0, 1);
